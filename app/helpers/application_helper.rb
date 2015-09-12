2fb6de5e9a9a2973e1af02c10f8b457d938c63d3
Duyet Nguyen <duyet.nguyen@outlook.com>
2015-09-13 00:16:07 +0700

    update flash errors messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,9 @@ module ApplicationHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: chef.name, class: "avatar")
   end
+
+  def full_title title
+    base_title = "Recipes Pro"
+    title.empty? ? base_title : "#{title} | #{base_title}"
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,5 @@ class Recipe < ActiveRecord::Base
 
   validates :title, presence: true, length: {minimum: 6, maximum: 100}
   validates :description, presence: true, length: {minimum: 10, maximum: 500}
+  validates :chef_id, presence: true
 end
