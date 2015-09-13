a054f3d123d3cf3b93724cf5c35f1f5e6995e22c
Duyet Nguyen <duyet.nguyen@outlook.com>
2015-09-13 09:28:16 +0700

    using carriewave cloudinay for recipe img demo

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,8 @@
 class Recipe < ActiveRecord::Base
   belongs_to :chef
 
+  mount_uploader :img, RecipeUploader
+
   validates :title, presence: true, length: {minimum: 6, maximum: 100}
   validates :description, presence: true, length: {minimum: 10, maximum: 500}
   validates :chef_id, presence: true
